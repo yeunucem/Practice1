b7908041b71cb2a6f934b29fc13c7f7c7b7d6ea1
yeunucem <purplejy2331@gmail.com>
2025-03-31 18:42:54 +0900

    Add files via upload

--- com/salesforce4/ex2/Problem05.java
+++ com/salesforce4/ex2/Problem05.java
@@ -1,18 +1,31 @@
 package com.salesforce4.ex2;
 
+import java.util.Iterator;
+import java.util.Random;
 import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.generic.IREM;
 
 public class Problem05 {
 
 	public static void main(String[] args) {
 		// 매스랜덤,  활용; 배열을 사용하여 1~45까지의 숫자 중 임의의 6개의 숫자를 출력하세요.
 	    // continue, break 활용; 배열 6개
-		if //if문으로 배열 6자리 만들고, 중복되지 않도록 제한한 후에 매스랜덤으로 6자리 뽑기
 		
-		int num = (int)(Math.random()*45) + 1;
-		int[] randomNum = {}; //배열 선언
+		int[] num = new int[6];
+		Random rand = new Random();
 		
-		System.out.println(randomNum[]);
+		for(int i=0;i<6;i++) {
+			while(true) {
+				int randNum = (int)(Math.random()*45) + 1;
+			for(num1 ==num2)
+			if(int num2 == int num1) {
+				break Outter;
+				}
+			}
+		}
+				
+		System.out.println(new int[6]);
 
 	}
 
